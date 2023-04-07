a6887660084b6d09603b3fd704ee62a14b48a293
Ying Sin <yingsin0223@gmail.com>
2023-04-07 11:41:43 +0800

    added BottomAppBar

--- app/src/main/java/com/example/foodhub/MainActivity.kt
+++ app/src/main/java/com/example/foodhub/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.foodhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.foodhub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    
+
+    private lateinit var bindingMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        bindingMain = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        bindingMain.bottomNavView.background = null
+        bindingMain.bottomNavView.menu.getItem(2).isEnabled = false
     }
 }
