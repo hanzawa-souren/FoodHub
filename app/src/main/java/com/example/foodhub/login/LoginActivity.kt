a15a895b8ba2ed465cee46f2badeb81faa2ca66c
Ying Sin <yingsin0223@gmail.com>
2023-05-09 21:28:32 +0800

    commit 2

--- app/src/main/java/com/example/foodhub/login/LoginActivity.kt
+++ app/src/main/java/com/example/foodhub/login/LoginActivity.kt
@@ -1,23 +1,15 @@
 package com.example.foodhub.login
 
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.example.foodhub.R
 
 class LoginActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
     }
 
 }
